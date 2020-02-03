6d2c5e53e9365512d72bdaf1c7def67c4e8c61de
GuerrazYohan <yohanguerraz1999@gmail.com>
2020-02-03 22:46:00 +0100

    YG-ADD : Istallation d un Framework pour relier la bdd au Xamarin

--- APPLICATION/AwesomeApp/AwesomeApp.Android/obj/Debug/90/android/environment.x86_64.s
+++ APPLICATION/AwesomeApp/AwesomeApp.Android/obj/Debug/90/android/environment.x86_64.s
@@ -62,7 +62,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"8b03f57f-c47f-4cf0-88a3-a0a3fd559e18"
+	.asciz	"ddc2caa2-b60a-4134-88bc-1737000c16ab"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
