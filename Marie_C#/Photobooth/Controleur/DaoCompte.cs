052969b5b22ff87387224a4c92f0d095635e5f34
Netiizy <teomarie88@gmail.com>
2020-02-03 15:25:55 +0100

    V1.0

---
+++ Marie_C#/Photobooth/Controleur/DaoCompte.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections;
+using System.Data.Entity;
+
+namespace Photobooth.Controleur
+{
+    class DaoCompte
+    {
+        private photoboothproject_2020Entities context;
+
+        ////////////READ///////////
+        public IEnumerable<object> GetCompte()
+        {
+            using (context = new photoboothproject_2020Entities())
+            {
+                var MonCompte = (from d in context.compte select d).ToList();
+                return MonCompte;
+            }
+        }
+    }
+}
---
+++ Marie_C#/Photobooth/Controleur/DaoEvenements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections;
+using System.Data.Entity;
+
+
+namespace Photobooth.Controleur
+{
+    class DaoEvenements
+    {
+        private photoboothproject_2020Entities context;
+        public IEnumerable<object> GetEvenements()
+        {
+            using (context = new photoboothproject_2020Entities())
+            {
+                var MesEvenement = (from d in context.evenements select d).ToList();
+                return MesEvenement;
+            }
+        }
+    }
+}
