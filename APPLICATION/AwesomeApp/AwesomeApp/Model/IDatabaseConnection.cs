5104310961c6cecce97fcee0dde09600fdb26ab5
GuerrazYohan <yohanguerraz1999@gmail.com>
2020-02-04 10:30:41 +0100

    YG-UPD : Mise a jour Framework pour relier la bdd au Xamarin

---
+++ APPLICATION/AwesomeApp/AwesomeApp/Model/IDatabaseConnection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SQLite;
+using System.Text;
+using LocalDataAccess.Droid;
+using AwesomeApp.Model;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DatabaseConnection_Android))]
+
+
+
+namespace AwesomeApp.Model
+{
+    interface IDatabaseConnection
+    {
+        SQLite.SQLiteConnection DbConnection();
+    }
+}
+
+
+
+
+namespace LocalDataAccess.Droid
+{
+    public class DatabaseConnection_Android : IDatabaseConnection
+    {
+
+        public SQLiteConnection DbConnection()
+        {
+            var dbName = "PhotoBoothProject_2020.db3";
+            var path = Path.Combine(System.Environment.
+              GetFolderPath(System.Environment.
+              SpecialFolder.Personal), dbName);
+            return new SQLiteConnection(path);
+        }
+    }
+
+    public class Compte
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string email { get; set; }
+        public int password { get; set; }
+
+    }
+}
+
+
+
+
