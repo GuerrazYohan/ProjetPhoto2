1e731798bde98bd4aad77aa93e118896de424466
Netiizy <teomarie88@gmail.com>
2020-02-04 11:47:02 +0100

    V1.0

--- Marie_C#/Photobooth/MainWindow.xaml.cs
+++ Marie_C#/Photobooth/MainWindow.xaml.cs
@@ -12,10 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using Photobooth.Controleur;
 using Photobooth.View;
 
 namespace Photobooth
 {
+
+    //DaoCompte DaoLogin = new DaoCompte();
+    //compte MonCompte = new compte();
+    //evenements MesEvenements = new evenements();
+
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
@@ -26,6 +33,34 @@ namespace Photobooth
             InitializeComponent();
             UC_Login my_Login = new UC_Login();
             Window_Container.Children.Add(my_Login);
+        }
+
+        private void Btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+            //if (MonCompte != null && MesEvenements != null)
+            //{
+            //if(tb_Username.Text = MonCompte.Email && tb_Mdp.Password = MesEvenements.MotDePasseEvent)
+            //{
+
+            //}
+            //DaoLogin.GetCompte();
+            //string Username = MonCompte.Email;
+            //string Password = MesEvenements.MotDePasseEvent;
+
+            //string Username = Marie;
+            //string Password = 1234;
+            //Convert.ToInt32(Username);
+            //Convert.ToInt32(Password);
+
+            //if (tb_Username.Text == Username && tb_Mdp.Text == Password)
+            //{
+
+            //}
+            //}
+            Window_Container.Children.Clear();
+            UC_Home my_Home = new UC_Home();
+            Window_Container.Children.Add(my_Home);
+            btn_Login.Visibility = Visibility.Hidden;
         }
     }
 }
