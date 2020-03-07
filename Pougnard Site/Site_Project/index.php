12b7163c2dd497af1f949ea3dbe863ee391a73b7
ZataRion <corentin.pougnard@gmail.com>
2020-03-07 22:04:32 +0100

    V1.97

--- Pougnard Site/Site_Project/index.php
+++ Pougnard Site/Site_Project/index.php
@@ -29,6 +29,7 @@
     </div>
   </nav>
 
+
   <div class="parallax-container">
    <div class="parallax">
      <img src="View/img/Code.jpg" alt="" class="responsive-img">
@@ -64,10 +65,10 @@
       <div class="col s12 l8">
         <h5>A propos</h5>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Le but de se site est de permettre à un particulier de louer un objet nommer "PhotoBooth" durant une durée déterminée.
+          Il faut d'abbord s'inscrire sur le site pour accéder a votre panneaux de contrôle
+          Depuis celui-ci vous aurez accés nous seulement au photos que vous auriez pu prendre durant un événements.
+          Ou encore accés à la demande de résérvation de l'appareil.
         </p>
       </div>
       <div class="col s12 l4 offset-12">
